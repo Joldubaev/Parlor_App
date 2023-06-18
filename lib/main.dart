637223c492da2b7c72e7df51163a90aa57d67ca4
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-06-18 23:57:25 +0600

    add zh splash screen exs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'app/app_main.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds: 3));
 }
