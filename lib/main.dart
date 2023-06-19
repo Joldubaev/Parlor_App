8f48de47db18de6e8f7bf3641905f2b981b60eb8
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2023-06-19 13:21:54 +0600

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app/app_main.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
